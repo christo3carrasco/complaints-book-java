5f8395a82d935f9db4ac845aaae124f2ddc7dda9
Christopher Carrasco <christopher3carrasco@gmail.com>
2023-03-06 22:03:08 -0500

    Initial commit

---
+++ src/test/java/com/carrasco/complaintsbook/ComplaintsBookJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrasco.complaintsbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintsBookJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
