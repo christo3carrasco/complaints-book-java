21f4b2f2223e620195ba5d27a683bf9652f00b46
Christopher Carrasco <christopher3carrasco@gmail.com>
2023-03-10 20:24:47 -0500

    feat: add complaint dto

---
+++ src/main/java/com/carrasco/complaintsbook/domain/dto/ComplaintDTO.java
@@ -0,0 +1,12 @@
+package com.carrasco.complaintsbook.domain.dto;
+
+import lombok.Data;
+
+@Data
+public class ComplaintDTO {
+    private int id;
+    private String place;
+    private String type;
+    private String comment;
+    private int user;
+}
